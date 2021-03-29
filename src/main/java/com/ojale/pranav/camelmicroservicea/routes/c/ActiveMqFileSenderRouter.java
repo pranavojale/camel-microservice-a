18362320b4e551913abb59a1d054d67ab32ed798
pranavoj <pranavojale@gmail.com>
2021-03-29 13:40:17 +0530

    Working integration with processor and transformer

--- src/main/java/com/ojale/pranav/camelmicroservicea/routes/c/ActiveMqFileSenderRouter.java
+++ src/main/java/com/ojale/pranav/camelmicroservicea/routes/c/ActiveMqFileSenderRouter.java
@@ -3,16 +3,12 @@ package com.ojale.pranav.camelmicroservicea.routes.c;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-//@Component
-public class ActiveMqSenderRouter extends RouteBuilder {
+@Component
+public class ActiveMqFileSenderRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        // timer
-        from("timer:active-mq-timer?period=10000")
-                .transform().constant("My message for Active MQ")
+        from("file:files/json")
                 .log("${body}")
                 .to("rabbitmq://localhost:5672/pranavoj?queue=my-rabbitmq-queue&autoDelete=false");
-                //.to("rabbitmq:pranavoj?queue=my-rabbitmq-queue");
-        // queue
     }
 }
